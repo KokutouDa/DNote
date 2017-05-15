5ba3903046882091db3e5fb6554fcef47431d600
kokutouda <kokutouda@gmail.com>
2017-05-15 08:00:27 +0800

    navigation drawer

--- app/src/main/java/com/kokutouda/dnote/MainActivity.java
+++ app/src/main/java/com/kokutouda/dnote/MainActivity.java
@@ -3,6 +3,8 @@ package com.kokutouda.dnote;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        //没有这个方法就显示不了 左上角的三条杠
+//        toggle.syncState();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //填充右上角的设置
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //右上角的设置
+    //@SuppressWarning 抑制警告，当里面没有 item 时不会报错
+    @SuppressWarnings("StateMent")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
