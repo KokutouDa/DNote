ff763ba638a011a6a14e23d7ff20672578dfd1d0
KokutouDa <kokutouda@gmail.com>
2018-09-19 16:54:42 +0800

    Revert "封装RecyclerView.OnItemTouchListener"
    This reverts commit 790cae812c089bbe28adde832c67fbc363cb7448.

--- app/src/main/java/com/kokutouda/dnote/dnote/ui/MainActivity.java
+++ app/src/main/java/com/kokutouda/dnote/dnote/ui/MainActivity.java
@@ -69,12 +69,45 @@ public class MainActivity extends AppCompatActivity
 
         //RecyclerView
         mRecyclerView = findViewById(R.id.recyclerView);
+<<<<<<< HEAD
 //        mRecyclerView.addOnItemTouchListener(new NotesListOnItemTouchListener(this, new NotesListOnItemTouchListener.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(View v, int position) {
 //                startNotesAtyForResult(position);
 //            }
 //        }));
+=======
+        mRecyclerView.addOnItemTouchListener(new NotesListOnItemTouchListener(new NotesListOnItemTouchListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v) {
+                int position = mRecyclerView.findContainingViewHolder(v).getAdapterPosition();
+                startNotesAtyForResult(position);
+//                return true;
+            }
+        }));
+//        mRecyclerView.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
+//            @Override
+//            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+//                if (e.getAction() == MotionEvent.ACTION_UP) {
+////                    mRecyclerView.getChildLayoutPosition();
+//                    View itemView = mRecyclerView.findChildViewUnder(e.getX(), e.getY());
+//                    if (itemView != null) {
+//
+//                        int position = mRecyclerView.findContainingViewHolder(itemView).getAdapterPosition();
+//                        startNotesAtyForResult(position);
+//                        return true;
+//                    }
+//                };
+//                return false;
+//
+//            }
+//
+//            @Override
+//            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+//                super.onTouchEvent(rv, e);
+//            }
+//        });
+>>>>>>> parent of 790cae8... 封装RecyclerView.OnItemTouchListener
     }
 
     public void initData() {
--- app/src/main/java/com/kokutouda/dnote/dnote/ui/NotesListOnItemTouchListener.java
+++ app/src/main/java/com/kokutouda/dnote/dnote/ui/NotesListOnItemTouchListener.java
@@ -1,46 +1,40 @@
 package com.kokutouda.dnote.dnote.ui;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 
 public class NotesListOnItemTouchListener implements RecyclerView.OnItemTouchListener {
-
-    public interface OnItemClickListener {
-        void onItemClick(View v, int position);
-    }
-
     private OnItemClickListener mClickListener;
-    private GestureDetector mGestureDetector;
 
-    public NotesListOnItemTouchListener(Context context, OnItemClickListener onItemClickListener) {
+    public NotesListOnItemTouchListener(OnItemClickListener onItemClickListener) {
         mClickListener = onItemClickListener;
-        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return true;
-            }
-        });
     }
 
     @Override
     public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-        View itemView = rv.findChildViewUnder(e.getX(), e.getY());
-        if (mClickListener != null && itemView != null && mGestureDetector.onTouchEvent(e)) {
-            int position = rv.getChildAdapterPosition(itemView);
-            mClickListener.onItemClick(itemView, position);
-            return true;
+        if (e.getAction() == MotionEvent.ACTION_UP) {
+            View itemView = rv.findChildViewUnder(e.getX(), e.getY());
+            if (mClickListener != null && itemView != null) {
+                mClickListener.onItemClick(itemView);
+                return true;
+            }
         }
         return false;
     }
 
     @Override
-    public void onTouchEvent(RecyclerView rv, MotionEvent e) { }
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
 
     @Override
     public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
 
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View v);
     }
 }
